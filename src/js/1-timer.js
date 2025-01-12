2e5ae54a91444676f42a0796012419e39ee72eeb
Oksana Molchanova <mksenia728@gmail.com>
2025-01-12 20:16:31 +0100

    fix

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -28,7 +28,7 @@ const validateDate = date => {
     iziToast.show({
       position: 'topRight',
       messageColor: 'white',
-      iconUrl: '../img/bi_x-octagon.png',
+      iconUrl: '../img/icon-error.svg',
       backgroundColor: '#EF4040',
       message: 'Please choose a date in the future',
     });
